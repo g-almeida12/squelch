c6201ae2b4a6b2981adfd40342ecc83dd113cbd3
GabAlmeyda <gabrielalmeida7258@gmail.com>
2026-07-05 20:51:55 -0300

    fix(client): added visual feedback for completed challenges on Sidebar component.

--- apps/client/src/components/Sidebar/Sidebar.tsx
+++ apps/client/src/components/Sidebar/Sidebar.tsx
@@ -3,9 +3,11 @@ import { RiMenuFold2Fill } from "react-icons/ri";
 import { useEffect, useState } from "react";
 import { useGetChallengeList } from "../../features/challenge/hooks/challenges.queries";
 import { ChallengeNavbar } from "./components";
+import { Link } from "react-router-dom";
+import { APP_ROUTES } from "../../config/constants";
 
 export function Sidebar() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   const { data: challengeList, isFetching, isError } = useGetChallengeList();
 
   useEffect(() => {
@@ -22,12 +24,31 @@ export function Sidebar() {
     <>
       <FocusScope trapped={isOpen} loop={isOpen} asChild>
         <aside
-          className={`transition-all fixed z-101 top-0 bottom-0 flex flex-col gap-4 overflow-x-hidden overflow-y-auto w-13 p-2 bg-surface ${isOpen ? "w-65" : "w-13"}`}
+          className={`transition-all fixed z-101 top-0 bottom-0 flex flex-col overflow-x-hidden overflow-y-auto w-13 p-2 bg-surface ${isOpen ? "w-65" : "w-13"}`}
           id="sidebar"
           aria-label="Menu lateral de desafios"
         >
+          {/* Logo */}
+          <div
+            className="flex flex-row items-center justify-start gap-2 border-b border-solid pb-2 border-subtle"
+            aria-hidden="true"
+          >
+            <Link
+              to={APP_ROUTES.HOME}
+              className="size-9 min-w-9 rounded-full bg-accent-primary flex justify-center items-center select-none"
+            >
+              <div className="relative -top-px h-4 flex justify-center items-center font-bold font-heading text-tx-contrast text-sm">
+                SQL
+              </div>
+            </Link>
+
+            <span className="font-heading text-base text-accent-primary font-medium select-none">
+              Squelch
+            </span>
+          </div>
+
           {/* Toggle button */}
-          <div className="absolute bottom-0 left-2 right-2 flex items-center justify-end border-t border-solid p-1 border-subtle">
+          <div className="flex items-center justify-start mb-3.5 border-b border-solid p-1 border-subtle">
             <button
               onClick={() => setIsOpen((prev) => !prev)}
               className="p-1 cursor-pointer"
@@ -37,26 +58,10 @@ export function Sidebar() {
             >
               <RiMenuFold2Fill
                 size={24}
-                className={`text-tx-overlay ${isOpen ? "rotate-180" : ""} min-w-6`}
+                className={`text-tx-overlay transition-transform duration-150 ${isOpen ? "scale-x-[-1]" : "scale-x-100"} min-w-6`}
                 aria-hidden="true"
               />
             </button>
-          </div>
-
-          {/* Logo */}
-          <div
-            className="flex flex-row items-center justify-start gap-2 border-b border-solid pb-2 border-subtle"
-            aria-hidden="true"
-          >
-            <div className="size-9 min-w-9 rounded-full bg-accent-primary flex justify-center items-center select-none">
-              <div className="relative -top-px h-4 flex justify-center items-center font-bold font-heading text-tx-contrast text-sm">
-                SQL
-              </div>
-            </div>
-
-            <span className="font-heading text-base text-accent-primary font-medium select-none">
-              Squelch
-            </span>
           </div>
 
           {/* Fallback logic */}
@@ -67,17 +72,23 @@ export function Sidebar() {
 
             if (isError) {
               if (isOpen) {
-                return <p className="text-tx-overlay">Não foi possível carregar os desafios.</p>;
+                return (
+                  <p className="text-tx-overlay">
+                    Não foi possível carregar os desafios.
+                  </p>
+                );
               } else {
                 return <div></div>;
               }
             }
 
             return (
-              <ChallengeNavbar
-                challengeList={challengeList!}
-                isSidebarOpen={isOpen}
-              />
+              <>
+                <ChallengeNavbar
+                  challengeList={challengeList!}
+                  isSidebarOpen={isOpen}
+                />
+              </>
             );
           })()}
         </aside>
--- apps/client/src/components/Sidebar/components/ChallengeNavbar.tsx
+++ apps/client/src/components/Sidebar/components/ChallengeNavbar.tsx
@@ -10,22 +10,20 @@ interface ChallengeNavbarProps {
   isSidebarOpen: boolean;
 }
 
-export function ChallengeNavbar({ challengeList, isSidebarOpen }: ChallengeNavbarProps) {
+export function ChallengeNavbar({
+  challengeList,
+  isSidebarOpen,
+}: ChallengeNavbarProps) {
   const [openGroups, setOpenGroups] = useState<Record<string, boolean>>({});
-  
+
   return (
     <nav aria-label="Navegação de desafios">
-      <ul className="flex flex-col gap-4">
+      <ul className="flex flex-col gap-1.5">
         {Object.entries(challengeList).map(([groupTitle, challenges]) => {
           const Icon = GROUP_ICONS[groupTitle];
 
           return (
-            <li
-              className="group"
-              key={groupTitle}
-              id={groupTitle}
-              data-open={openGroups[groupTitle] ? "true" : "false"}
-            >
+            <li className="group" key={groupTitle} id={groupTitle}>
               {/* Group title */}
               <div className="relative z-1 flex flex-row items-center p-1 select-none justify-start">
                 <div className="flex flex-row gap-1 items-center">
@@ -36,7 +34,7 @@ export function ChallengeNavbar({ challengeList, isSidebarOpen }: ChallengeNavba
                   />
                   <strong
                     aria-hidden={!isSidebarOpen}
-                    className={`text-accent-primary font-medium text-base line-clamp-1 text-ellipsis ${isSidebarOpen ? "" : "hidden"}`}
+                    className={`text-accent-primary font-heading font-semibold text-base line-clamp-1 text-ellipsis ${isSidebarOpen ? "" : "hidden"}`}
                   >
                     {groupTitle}
                   </strong>
@@ -56,32 +54,40 @@ export function ChallengeNavbar({ challengeList, isSidebarOpen }: ChallengeNavba
                   disabled={!isSidebarOpen}
                 >
                   <FaCaretUp
-                    className={`text-accent-primary ${isSidebarOpen ? "" : "hidden"} rotate-180 group-data-[open=true]:rotate-0`}
+                    className={`text-accent-primary ${isSidebarOpen ? "" : "hidden"} ${openGroups[groupTitle] ? "rotate-0" : "rotate-180"}`}
                     size={20}
                     aria-hidden={true}
                   />
                 </button>
-                <div className="absolute top-0 right-0 bottom-0 left-0 opacity-20 -z-1 bg-accent-primary"></div>
+                <div className="absolute top-0 right-0 bottom-0 left-0 -z-1 bg-accent-primary/30"></div>
               </div>
 
               {/* Challenges */}
               <ul
-                className="hidden flex-col items-start gap-0.5 mt-2 select-none group-data-[open=true]:flex"
+                className={`flex-col items-start select-none ${openGroups[groupTitle] && isSidebarOpen ? `flex` : "hidden"}`}
                 id={`list-${groupTitle}`}
               >
-                {challenges.map((c) => {
+                {challenges.map((c, idx) => {
                   return (
                     <li
-                      className="flex flex-row gap-2 w-full h-7.5"
+                      className={`flex flex-row items-center w-full h-7.5 border-l-2 font-heading ${
+                        c.completedByUser
+                          ? "border-accent-primary bg-accent-primary/10"
+                          : "border-transparent bg-dark/60"
+                      }`}
                       data-id={c.id}
                       key={c.id}
                     >
                       <div className="h-full w-9 min-w-9 shrink-0"></div>
                       <Link
                         to={APP_ROUTES["CHALLENGE"](c.id)}
-                        className="w-full block truncate text-tx-overlay text-[15px] line-clamp-1 text-left cursor-pointer"
+                        className={`w-full block truncate text-[15px] line-clamp-1 text-left cursor-pointer ${
+                          c.completedByUser
+                            ? "text-tx-main font-semibold"
+                            : "text-tx-overlay font-normal"
+                        }`}
                       >
-                        {c.title}
+                        {idx + 1}. {c.title}
                       </Link>
                     </li>
                   );
