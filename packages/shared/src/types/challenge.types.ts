ba34648698ff56c972d50014aa438c005a349886
GabAlmeyda <gabrielalmeida7258@gmail.com>
2026-07-09 13:00:54 -0300

    fix(server): commited missing file for the last feat(server)

--- packages/shared/src/types/challenge.types.ts
+++ packages/shared/src/types/challenge.types.ts
@@ -1,13 +1,26 @@
-import type { Id, ChallengeDTO } from "../schemas";
+import type { Id } from "../schemas";
 
-export type ChallengeResume = Omit<ChallengeDTO, "markdown"> & {
+export type ChallengeDifficulties = "EASY" | "MEDIUM" | "HARD";
+
+export type ChallengeDTO = {
+  title: string;
+  markdown: string;
+  groupSlug: string;
+  groupTitle: string;
+  difficulty: ChallengeDifficulties;
+  completedByUser: boolean;
+  id: Id;
+};
+
+export type ChallengeResume = Omit<
+  ChallengeDTO,
+  "markdown" | "completedByUser"
+> & {
   userId: Id;
   totalSubmissions: number;
   lastSubmissionDate: Date;
 };
 
-export type ChallengeListItem = Omit<ChallengeDTO, "markdown"> & {
-  completedByUser: boolean;
-};
+export type ChallengeListItem = Omit<ChallengeDTO, "markdown">;
 
 export type ChallengeList = Record<string, ChallengeListItem[]>;
